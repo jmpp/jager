ba5b146db379255875f058436d8d97258b8845e4
Emilien REGENT <emilien.regent@gmail.com>
2015-04-11 11:17:02 +0200

    Add configuration system

---
+++ public/conf/player.cfg.js
@@ -0,0 +1,8 @@
+var PlayerConf = {
+	position :  {x : 0,	y : 0},
+	scaleMin :  {x : 5,	y : 5},
+	scaleMax :  {x : 30, y : 30},
+	speedMin :  {x : 1,	y : 1},
+	speedMax :  {x : 3,	y : 3},
+	transformDuration : 500
+}
--- public/js/game.js
+++ public/js/game.js
@@ -3,6 +3,7 @@ var Game = function()
 	var instance = this;
 
 	this.players = {};
+	this.configurations = {};
 
 	this.inititialize = function()
 	{
@@ -23,19 +24,62 @@ var Game = function()
 			})();
 		}
 
+		this.configure();
+
 		this.update();
+	}
+
+	this.configure = function()
+	{
+		this.getConfiguration("Player");
+	}
+
+	this.getConfiguration = function(name)
+	{
+		if(this.configurations[name] === undefined)
+		{
+		    var head 	= document.getElementsByTagName('head')[0];
+		    var script 	= document.createElement('script');
+		    script.type = 'text/javascript';
+		    script.src 	= "conf/" + name + ".cfg.js";
+
+		    var callback = function() { instance.setConfiguration(name);}
+
+		    script.onreadystatechange 	= callback;
+		    script.onload 				= callback;
+
+		    head.appendChild(script);
+		}
+		else
+		{
+			return this.configurations[name];
+		}
+	}
+
+	this.setConfiguration = function(name)
+	{
+		var confName = ucfirst(name) + "Conf";
+
+		if(window[confName] !== undefined)
+		{
+
+			this.configurations[name] = window[confName];
+
+			window[confName] = undefined;
+		}
+		else
+		{
+			console.error("[Game] Configuration for " + name + " doesn't exit in the current context !");
+		}
 	}
 
 	this.update = function()
 	{
-
 		canvas.drawRect(canvas);
 
 		for(token in instance.players)
 		{
-			var player = instance.players[token];
-
-			canvas.drawRect(player);
+			instance.players[token].update();
 		}
 
 		requestAnimationFrame(instance.update);
@@ -76,6 +120,32 @@ var Game = function()
 		else
 		{
 			console.error("[Game] Try to move an undefined player with token " + token + " !");
+		}
+	}
+
+	this.transformPlayer = function(token)
+	{
+		if(token === undefined)
+		{
+			console.error("[Game] Try to transform player without a valid token !");
+		}
+
+		if(this.players[token] !== undefined)
+		{
+			var player = this.players[token];
+
+			if(player.isJager === true)
+			{
+				player.transformToPrey();
+			}
+			else
+			{
+				player.transformToJager();
+			}
+		}
+		else
+		{
+			console.error("[Game] Try to transform an undefined player with token " + token + " !");
 		}
 	}
 }
