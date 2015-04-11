79e4a42f9f57d7e58244624e02e1df81c88900b9
jmpp <jmclery@gmail.com>
2015-04-11 09:28:45 +0200

    Send setToken to a client

--- websockets.js
+++ websockets.js
@@ -1,27 +1,51 @@
 'use strict';
 
-var io = require('socket.io');
+var _       = require('underscore');
+var io      = require('socket.io');
+var shortid = require('shortid');
 
-var singleton = function socketsController(server) {
-  if (singleton.sockets)
-    return;
-  
+// var Player  = require('./class/Player');
+
+var playersList = [];
+
+// Main controller
+// ---------------
+function socketsController(server) {
   var ws = io(server);
-  singleton.sockets = ws.sockets;
-
-  // URL for client : /socket.io/socket.io.js
 
   // Once a client connect to server
   ws.on('connection', function(socket) {
 
-    socket.on('createPlayer', createPlayer);
+    // When client sets a new player name
+    // ----------------------------------
+    socket.on('createPlayer', function createPlayer(name) {
+      console.log(
+        'New socket from client !'.green,
+        'createPlayer()'.magenta,
+        ('"' + name.toString() + '"').cyan
+      );
+
+      // Check if player already exists within array
+      /*var player = _.findWhere(playersList, { name: name });
+      if (player)
+        return console.error('Name ', name, 'is already taken');*/
+
+      /*playersList.push(new Player(
+        shortid.generate(),
+        name,
+
+      })*/
+      
+      var token = shortid.generate();
+      playersList.push({
+        token: token,
+        name : name
+      });
+
+      socket.emit('setToken', { token: token });
+    }
 
   });
-};
-
-// When client sets a new player name
-function createPlayer(name) {
-  console.log('New socket from client !'.green, 'createPlayer()'.magenta, name.toString().cyan);
 }
 
-module.exports = singleton;
+module.exports = socketsController;
