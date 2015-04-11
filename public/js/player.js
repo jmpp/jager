7eb7752a853ed1d5aab64f9f00441034d41e98be
Emilien REGENT <emilien.regent@gmail.com>
2015-04-11 11:17:24 +0200

    Add transform system for player

--- public/js/player.js
+++ public/js/player.js
@@ -1,17 +1,96 @@
 var Player = function(args)
 {
 	var args = args === undefined ? {} : args;
+	var conf = game.getConfiguration("Player");
 
 	this.name		= args.name !== undefined ? args.name : null;
 	this.color		= args.color !== undefined ? args.color : new Vector4(255,255,255,1);
 
 	this.position 	= new Vector2(canvas.position.x + canvas.scale.x / 2, canvas.position.y + canvas.scale.y/2);
-	this.scale 		= new Vector2(5,5);
-	this.speed		= new Vector2(1,1);
+	this.scale 		= new Vector2(conf.scaleMin.x, conf.scaleMin.y);
+	this.speed		= new Vector2(conf.speedMin.x, conf.speedMin.y);
+
+	this.isJager		= false;
+	this.isTransform	= false;
+	this.canMove		= true;
+
+	this.transformTime		= 0;
+	this.transformDuration 	= conf.transformDuration;
+
+	this.update = function()
+	{
+		this.render();
+
+		if(this.isTransform === true)
+		{
+			this.isTransform = this.transform();
+		}
+	}
+
+	this.render = function()
+	{
+		canvas.drawRect(this);
+	}
 
 	this.move = function(position)
 	{
-		this.position.x += this.speed.x * position.x;
-		this.position.y += this.speed.y * position.y;
+		if(this.canMove === true)
+		{
+			this.position.x += this.speed.x * position.x;
+			this.position.y += this.speed.y * position.y;
+		}
+	}
+
+	this.transform = function()
+	{
+		var time = new Date().getTime() - this.transformTime;
+
+		if(time > this.transformDuration)
+		{
+			this.isJager = !this.isJager;
+
+			return false;
+		}
+
+		var conf 		= game.getConfiguration("Player");
+		var ratio		= time / this.transformDuration;
+		var scale 		= {x: conf.scaleMin.x + (conf.scaleMax.x - conf.scaleMin.x), y: conf.scaleMin.y + (conf.scaleMax.y - conf.scaleMin.y)};
+			scale 		= this.isJager === true 
+			? {x: scale.x * (1 - ratio), y: scale.y * (1 - ratio)}
+			: {x: scale.x * ratio, y: scale.y * ratio};
+
+		this.setScale(scale);
+
+		return true;
+	}
+
+	this.transformToPrey = function()
+	{
+		if(this.isTransform === false)
+		{
+			this.isTransform 	= true;
+
+			this.transformTime 	= new Date().getTime();
+		}
+	}
+
+	this.transformToJager = function()
+	{
+		if(this.isTransform === false)
+		{
+			this.canMove 		= false;
+			this.isTransform 	= true;
+
+			this.transformTime 	= new Date().getTime();
+		}
+	}
+
+	this.setScale = function(scale)
+	{
+		this.position.set(this.position.x + this.scale.x/2, this.position.y + this.scale.y/2);
+		
+		this.scale.set(scale.x.clamp(conf.scaleMin.x, conf.scaleMax.x), scale.y.clamp(conf.scaleMin.y, conf.scaleMax.y));
+
+		this.position.set(this.position.x - this.scale.x/2, this.position.y - this.scale.y/2);
 	}
 }
